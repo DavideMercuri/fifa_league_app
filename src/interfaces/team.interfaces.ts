3ba4c45ee164efac562b03011cb06422b268a864
Davide Mercuri <DavideMercuri@users.noreply.github.com>
2023-10-08 18:18:22 +0200

    added team.interfaces

---
+++ src/interfaces/team.interfaces.ts
@@ -0,0 +1,12 @@
+export interface Team {
+    team_id: string;
+    team_name: string;
+    money: number;
+    league_win: number;
+    cup_win: number;
+    players_number: number;
+    salary: number;
+    captain: string;
+    team_logo: string;
+    club_value: number;
+}
