07c21c693ec44aada724332695126ad758a33556
Davide Mercuri <DavideMercuri@users.noreply.github.com>
2023-11-27 23:18:32 +0100

    Added edit-player.component (to imp)

---
+++ src/app/views/players/players/edit-player/edit-player.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-player',
+  templateUrl: './edit-player.component.html',
+  styleUrls: ['./edit-player.component.less']
+})
+export class EditPlayerComponent {
+
+}
