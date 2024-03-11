54e8139f030f5ca9d0d42061e470b202069caf5c
Davide Mercuri <DavideMercuri@users.noreply.github.com>
2024-03-11 18:06:59 +0100

    added transaction logic to trophy-award-menu.component

--- src/app/views/team-detail/trophy-award-menu/trophy-award-menu.component.ts
+++ src/app/views/team-detail/trophy-award-menu/trophy-award-menu.component.ts
@@ -9,6 +9,7 @@ import { TuiAlertService, TuiNotification } from '@taiga-ui/core';
 import { TUI_DEFAULT_MATCHER, TuiContextWithImplicit, TuiIdentityMatcher, TuiStringHandler } from '@taiga-ui/cdk';
 import { Player } from 'src/interfaces/player.interface';
 import { Observable, Subject, delay, filter, of, startWith, switchMap } from 'rxjs';
+import { Transaction } from 'src/interfaces/transaction.interface';
 
 class PlayerSearch implements Player {
   constructor(
@@ -48,6 +49,7 @@ export class TrophyAwardMenuComponent implements OnInit {
   @Input() observer: any;
 
   selectedTeamPlayers: Player[] = [];
+  tempMoney: any;
 
   leagueInfo = leagueInfo;
   UCLInfo = UCLInfo;
@@ -71,15 +73,16 @@ export class TrophyAwardMenuComponent implements OnInit {
   pots: FormControl = new FormControl();
 
   ngOnInit(): void {
+    this.tempMoney = this.selectedTeam.money;
     this.filterPlayers(this.selectedTeam.team_name);
     this.pots.disable();
   }
 
-  setCheckboxStatus(){
-    if(this.awardForm.controls['awardGoldenBall'].value == false){
+  setCheckboxStatus() {
+    if (this.awardForm.controls['awardGoldenBall'].value == false) {
       this.pots.reset();
       this.pots.disable();
-    }else{
+    } else {
       this.pots.enable();
     }
   }
@@ -119,7 +122,7 @@ export class TrophyAwardMenuComponent implements OnInit {
   readonly identityMatcher: TuiIdentityMatcher<PlayerSearch> = (player1, player2) =>
     player1.id === player2.id;
 
-  save() {
+  async save() {
 
     let totalSum = 0;
 
@@ -132,20 +135,81 @@ export class TrophyAwardMenuComponent implements OnInit {
       awardGoldenBall: 5000000,
     };
 
-    Object.keys(this.awardForm.controls).forEach(key => {
+    const keys = Object.keys(this.awardForm.controls);
+    for (const key of keys) {
       const control = this.awardForm.get(key);
 
-      if (control) {
+      if (control && control.value) {
 
-        if (control.value) {
-          if (typeof control.value === 'object' && control.value.hasOwnProperty('awardValue')) {
-            totalSum += control.value.awardValue;
-          } else if (typeof control.value === 'boolean' && control.value === true) {
-            totalSum += checkboxValues[key];
-          }
+        var prizeType: string = '';
+        var prizeValue: number;
+
+        switch (key) {
+          case 'awardCampionato':
+            prizeType = `${control.value.label} Campionato`
+            prizeValue = control.value.awardValue;
+            break;
+          case 'awardChampionsLeague':
+            prizeType = `${control.value.label} UCL`
+            prizeValue = control.value.awardValue;
+            break;
+          case 'awardCup':
+            prizeType = `Vittoria Coppa`
+            prizeValue = 10000000;
+            break;
+          case 'awardBestScorer':
+            prizeType = `Premio Marcatore`
+            prizeValue = 15000000;
+            break;
+          case 'awardBestAssistman':
+            prizeType = `Premio Assist`
+            prizeValue = 10000000;
+            break;
+          case 'awardBestMotm':
+            prizeType = `Premio MOTM`
+            prizeValue = 5000000;
+            break;
+          case 'awardBestScorerUcl':
+            prizeType = `Premio Marcatore UCL`
+            prizeValue = 10000000;
+            break;
+          case 'awardGoldenBall':
+            prizeType = `Premio Pallone D'oro`
+            prizeValue = 5000000;
+            break;
+          default:
+            prizeType = ''
+            prizeValue = 0;
+            break;
+        }
+
+        this.tempMoney = this.tempMoney + prizeValue;
+
+        var transaction: Transaction =
+        {
+          transaction_mode: 'CREDIT',
+          transaction_amount: prizeValue,
+          transaction_date: new Date().toString(),
+          transaction_team: String(this.selectedTeam.team_name),
+          transaction_detail: {
+            type: 'prize',
+            prize: prizeType
+          },
+          updated_team_money: this.tempMoney
+        }
+
+        console.log(this.tempMoney);
+
+
+        await this.registerTransaction(this.selectedTeam.team_name, transaction);
+
+        if (typeof control.value === 'object' && control.value.hasOwnProperty('awardValue')) {
+          totalSum += control.value.awardValue;
+        } else if (typeof control.value === 'boolean' && control.value === true) {
+          totalSum += checkboxValues[key];
         }
       }
-    });
+    };
 
     var leagueWin: boolean = false;
     var uclWin: boolean = false;
@@ -159,10 +223,26 @@ export class TrophyAwardMenuComponent implements OnInit {
 
     this.updateMoney(Number(this.selectedTeam.team_id), totalSum, leagueWin, uclWin);
 
-    if(this.awardForm.controls['awardGoldenBall'].value == true){
+    if (this.awardForm.controls['awardGoldenBall'].value == true) {
       this.setBaloonDor(this.pots.value);
     }
 
+  }
+
+  registerTransaction(transaction_team: string, transaction: Transaction): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.http.post('http://localhost:3000/players/register-transaction', { teamName: transaction_team, transaction })
+        .subscribe({
+          next: (response) => {
+            console.log('Transaction registered successfully', response);
+            resolve(response);
+          },
+          error: (error) => {
+            console.error('Error registering transaction', error);
+            reject(error);
+          }
+        });
+    });
   }
 
   updateMoney(id: number, sum: number, leagueWin?: boolean, uclWin?: boolean) {
@@ -189,15 +269,15 @@ export class TrophyAwardMenuComponent implements OnInit {
     this.http.put('http://localhost:3000/players/team_detail/update-team-trophy', body).subscribe({});
   }
 
-  resetBaloonDor(){
-    
+  resetBaloonDor() {
+
     this.http.put('http://localhost:3000/players/team_detail/reset-baloon-dor', undefined).subscribe({});
 
   }
 
-  setBaloonDor(player: any){
+  setBaloonDor(player: any) {
 
-    this.resetBaloonDor();    
+    this.resetBaloonDor();
     this.http.put('http://localhost:3000/players/team_detail/set-baloon-dor', { id: player.id }).subscribe({
 
     });
